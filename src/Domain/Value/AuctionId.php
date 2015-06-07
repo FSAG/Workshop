e6d8618a011facb0baabdf1a01cc752a0258d1ba
Nikolai Zujev <nikolai.zujev@gmail.com>
2015-06-07 14:44:45 +0200

    auction and user ids

--- src/Domain/Value/AuctionId.php
+++ src/Domain/Value/AuctionId.php
@@ -2,7 +2,38 @@
 
 namespace Workshop\Auction\Domain\Value;
 
-class AuctionId
+final class AuctionId
 {
+    /**
+     * @var
+     */
+    private $value;
 
+    /**
+     * @return AuctionId
+     */
+    public static function generate()
+    {
+        $random = sprintf('a%s%s', time(),  mt_rand(100, 999));
+        return new self($random);
+    }
+
+    /**
+     * @param string $string
+     * @return AuctionId
+     */
+    public static function fromString($string)
+    {
+        return new self($string);
+    }
+
+    /**
+     * @todo add assertion
+     *
+     * @param $value
+     */
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
 }
--- src/Domain/Value/UserId.php
+++ src/Domain/Value/UserId.php
@@ -2,14 +2,38 @@
 
 namespace Workshop\Auction\Domain\Value;
 
-class UserId
+final class UserId
 {
+    /**
+     * @var
+     */
+    private $value;
+
     /**
      * @return UserId
      */
     public static function generate()
     {
-        return new UserId();
+        $random = sprintf('u%s%s', time(),  mt_rand(100, 999));
+        return new self($random);
     }
 
+    /**
+     * @param string $string
+     * @return UserId
+     */
+    public static function fromString($string)
+    {
+        return new self($string);
+    }
+
+    /**
+     * @todo add assertion
+     *
+     * @param $value
+     */
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
 }
